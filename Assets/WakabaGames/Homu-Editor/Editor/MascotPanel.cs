301754a0de2624f9fcd147c2abf838ded0a6a204
Teh Lemon <Teh-Lemon@users.noreply.github.com>
2018-05-15 00:31:38 +0100

    Mascot panel now supports transparencies and auto-scaling.

--- Assets/WakabaGames/Homu-Editor/Editor/MascotPanel.cs
+++ Assets/WakabaGames/Homu-Editor/Editor/MascotPanel.cs
@@ -1,5 +1,11 @@
 ﻿// Creates a panel in the Unity Editor that displays an image
-// Image needs to be called "mascot" and placed in "Assets/Resources/EditorResources"
+
+// Image needs to be called "mascot" and placed in "Assets/Editor Default Resources/WakabaGames/Homu-Editor/"
+// Image needs to be png, jpg or gif. You can add more below if you need them but it has to be a Unity supported format.
+
+// Material needs to be called "mascot" and placed in "Assets/Editor Default Resources/WakabaGames/Homu-Editor/"
+// The material isn't needed (though you'll get a warning when you first open the window) 
+// but the default material does not support transparencies
 
 using UnityEngine;
 using UnityEditor;
@@ -9,25 +15,32 @@ namespace WakabaGames.Editor
 {
     public class MascotPanel : EditorWindow
     {
-        static Texture2D m_texture = null;
+        static Texture2D m_texture;
+        static Material m_mat;
 
         [MenuItem ( "Window/Mascot ")]
         static void Open()
         {
             EditorWindow.GetWindow<MascotPanel>("Mascot");
-
-            //m_texture = Resources.Load("EditorResources/mascot") as Texture2D;
+            
+            // Load the mascot image from the "Assets/Editor Default Resources/WakabaGames/Homu-Editor/" folder
             m_texture = EditorGUIUtility.Load("WakabaGames/Homu-Editor/mascot.jpg") as Texture2D;
+            // try png if jpg isn't found
             if (m_texture == null)
             {
                 m_texture = EditorGUIUtility.Load("WakabaGames/Homu-Editor/mascot.png") as Texture2D;
             }
+            // try gif if png isn't found either
             if ( m_texture == null )
             {
                 m_texture = EditorGUIUtility.Load("WakabaGames/Homu-Editor/mascot.gif") as Texture2D;
             }
 
-            Assert.IsNotNull(m_texture, "Mascot Panel: Editor Default Resources/mascot not found");
+            Assert.IsNotNull(m_texture, "Mascot Panel: Editor Default Resources/WakabaGames/Homu-Editor/mascot.jpg/png/gif image not found");
+
+            // Load the mascot material
+            m_mat = EditorGUIUtility.Load("WakabaGames/Homu-Editor/mascot.mat") as Material;
+            Assert.IsNotNull(m_mat, "Mascot Panel: Editor Default Resources/WakabaGames/Homu-Editor/mascot.mat material not found");
         }
 
         void OnGUI()
@@ -37,27 +50,8 @@ namespace WakabaGames.Editor
                 return;
             }
 
-            // Size of the image to be displayed
-            var textureWidth = (float)m_texture.width;
-            var textureHeight = (float)m_texture.height;
-
-            if (position.width < textureWidth 
-                || position.height < textureHeight)
-            {
-                // If the image is larger than our window size 
-                var shrinkWidth = Mathf.Min(position.width / textureWidth, position.height / textureHeight);
-
-                // Re-size the image
-                textureWidth *= shrinkWidth;
-                textureHeight *= shrinkWidth;
-            }
-
-            // Centre of the window
-            var posX = (position.width - textureWidth) / 2;
-            var posY = (position.height - textureHeight) / 2;
-
             // Draw the image
-            EditorGUI.DrawPreviewTexture(new Rect(posX, posY, textureWidth, textureHeight), m_texture);
+            EditorGUI.DrawPreviewTexture(new Rect(0, 0, position.width, position.height), m_texture, m_mat, ScaleMode.ScaleToFit);
         }
     }
 }
