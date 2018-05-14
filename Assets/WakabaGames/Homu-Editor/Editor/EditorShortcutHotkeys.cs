dae9a044b63730d0e84da3c9e6d451d4a61373d2
Teh Lemon <Teh-Lemon@users.noreply.github.com>
2018-05-15 00:10:52 +0100

    Removed broken screenshot key Play and Deselect hotkeys now grouped together in the edit menu

--- Assets/WakabaGames/Homu-Editor/Editor/EditorShortcutHotkeys.cs
+++ Assets/WakabaGames/Homu-Editor/Editor/EditorShortcutHotkeys.cs
@@ -1,7 +1,6 @@
 // Adds extra hotkeys to the Unity Editor
 // F5 to Play and Stop the game
 // alt+D to deselect all gameobjects
-// F8 to screenshot
 
 #if UNITY_EDITOR
 using UnityEngine;
@@ -14,66 +13,23 @@ namespace WakabaGames.Editor
 {
     public class EditorShortCutKeys : ScriptableObject
     {
-        [MenuItem("Edit/Run _F5")] // shortcut key F5 to Play (and exit playmode also)
+        // F5 to Play (and exit playmode)
+        [MenuItem("Edit/Play _F5", false, 10001)] 
         static void PlayGame()
         {
             if (!Application.isPlaying)
             {
-                EditorSceneManager.SaveScene(SceneManager.GetActiveScene(), "", false); // optional: save before run
+                // save before playing
+                EditorSceneManager.SaveScene(SceneManager.GetActiveScene());
             }
             EditorApplication.ExecuteMenuItem("Edit/Play");
         }
 
-
-        [MenuItem("Edit/Deselect All &d", false, -101)]
+        // alt D to deselect gameobjects
+        [MenuItem("Edit/Deselect All &d", false, 10001)]
         static void Deselect()
-        {
+        {            
             Selection.activeGameObject = null;
-        }
-
-        [MenuItem("Edit/Take Screenshot _F8")]
-        static void TakeScreenshot()
-        {
-            DateTime d = DateTime.Now;
-            string game = Application.productName.Replace(" ", "_");
-            string scene = SceneManager.GetActiveScene().name.Replace(" ", "_");
-            string filename = $"{game}_{scene}_{d.Year}-{d.Month:d2}-{d.Day:d2}_{d.Hour:d2}-{d.Minute:d2}-{d.Second:d2}.png";
-            string filePath = EditorPrefs.GetString("MelonScreenshotSavePathKey", "").Replace("\\","\\\\");            
-            ScreenCapture.CaptureScreenshot(filePath + filename, 1);
-            Debug.Log("Screenshot taken");
-        }
-    }
-
-    public class MelonPreferences : MonoBehaviour
-    {    
-        // Have we loaded the prefs yet
-        static bool prefsLoaded = false;
-
-        // The Preferences
-        public static string MelonScreenshotSavePath = "";
-
-        // Add preferences section named "My Preferences" to the Preferences Window
-        [PreferenceItem("Melon Core")]
-        public static void PreferencesGUI()
-        {
-            // Load the preferences
-            if ( !prefsLoaded )
-            {
-                MelonScreenshotSavePath = EditorPrefs.GetString("MelonScreenshotSavePathKey", "");
-                prefsLoaded = true;
-                //Debug.Log($"Loaded screenshot path as {MelonScreenshotSavePath}");
-            }
-
-            // Preferences GUI
-            //GUI.Label("Leave blank to save in the project root folder");
-            MelonScreenshotSavePath = EditorGUILayout.TextField("Screenshot Save Path", MelonScreenshotSavePath);
-
-            // Save the preferences
-            if ( GUI.changed )
-            {
-                EditorPrefs.SetString("MelonScreenshotSavePathKey", MelonScreenshotSavePath);
-                //Debug.Log($"Melon settings saved. Screenshot path at {MelonScreenshotSavePath}.");
-            }
         }
     }
 }
